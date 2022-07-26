768fdea29b4d80b9d6186f4d371199b8793f30e2
zbob889 <zroberts889@gmail.com>
2022-07-26 13:19:01 +0200

    Got the ship sinking dom figured out

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,6 @@
+import addText from "./addText";
+import { players } from "./players";
+
 export const shipFactory = (name, length) => {
 
     let hitNumber = 0;
@@ -11,6 +14,15 @@ export const shipFactory = (name, length) => {
     function isSunk(){
         if(this.hitNumber == this.length){
             this.sunk = true;
+
+            const shipCells = this.boardLocation;
+
+            console.log(shipCells);
+
+            for(let i = 0; i < shipCells.length + 1; i++){
+                document.getElementById(`${shipCells[i]}`).className = 'diag';
+            }
+
             console.log('Ship sunk');
         } else if(hitNumber != length){
             this.sunk = false;
